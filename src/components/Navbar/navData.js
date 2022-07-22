f87ae98f469851448b3f2218c3df8c743c15577d
HonestDev0926 <strongpheonix102@gmail.com>
2022-07-23 01:13:02 +0300

    initial commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Github',
+    url: 'https://github.com/lovelydev315',
+    icon: 'github',
+  },
+];
